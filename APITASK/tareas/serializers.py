4250766b7e35de948d2ae0b56f3d64c37db2cbf1
aisaacbg <44000789+aisaacbg@users.noreply.github.com>
2021-02-08 06:17:10 -0600

    Delete APITASK directory

--- APITASK/tareas/serializers.py
+++
@@ -1,9 +0,0 @@
-from tareas.models import Tarea
-from rest_framework import serializers
-
-
-class TareaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tarea
-        '''fields = ['descripcion', 'duracion', 'tiempo_registrado', 'estatus']'''
-        fields= '__all__'
